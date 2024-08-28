20cb77bf2e87e985a64af2db0e5ab5fbafd80098
xingfunidengdengwo <2152288649@qq.com>
2024-08-28 21:56:39 +0800

    新增图表

--- src/views/charts.vue
+++ src/views/charts.vue
@@ -5,32 +5,57 @@ import { ElMessageBox, ElMessage } from 'element-plus';
 import * as echarts from 'echarts';
 onMounted(function () {
     // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('main'));
+    var myChart1 = echarts.init(document.getElementById('borrow'));
+    var myChart2 = echarts.init(document.getElementById('maintenance'));
+
+
     // 绘制图表
-    myChart.setOption({
+    myChart1.setOption({
         title: {
-            text: 'ECharts 示例'
+            text: '物品借出记录'
         },
         tooltip: {},
         xAxis: {
-            data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            data: ['笔记本电脑', '平板', '打印机', '投影仪', '机械键盘', '鼠标垫']
         },
         yAxis: {},
         series: [
             {
-                name: '销量',
+                name: '借出次数',
                 type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+                data: [5, 2, 12, 10, 8, 6]
             }
         ]
     });
 
-})
+
+
+    myChart2.setOption({
+        title: {
+            text: '物品维修记录'
+        },
+        tooltip: {},
+        xAxis: {
+            data: ['笔记本电脑', '平板', '打印机', '投影仪', '机械键盘', '鼠标垫']
+        },
+        yAxis: {},
+        series: [
+            {
+                name: '维修次数',
+                type: 'bar',
+                data: [5, 3, 3, 2, 3, 6]
+            }
+        ]
+    });
+
+});
 
 </script>
 
 <template>
-    <div id="main" style="width: 600px;height:400px;"></div>
+    <div id="borrow" style="width: 600px;height:400px;"></div>
+    <div id="maintenance" style="width: 600px;height:400px;"></div>
+
 </template>
 
 <style></style>
